bdca148ae30dd8ffbe50b2f689a82af249a2f41d
abdelboulgha <118727738+abdelboulgha@users.noreply.github.com>
2026-04-02 15:22:51 +0100

    whatsapp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import LayoutWrapper from "../components/LayoutWrapper";
+import WhatsAppButton from "../components/WhatsAppButton";
 
 export const metadata: Metadata = {
   title: "Argan Product by Sami | Coopérative Artisanale",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <LayoutWrapper>
           {children}
         </LayoutWrapper>
+        <WhatsAppButton />
       </body>
     </html>
   );
